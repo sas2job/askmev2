d3f3dafe836288f20357378b3f4e7d280b897b41
Alex Smirnov <sas2job@gmail.com>
2019-08-22 21:05:27 +0300

    Added comments for models and migrates

--- db/migrate/20190822171123_create_users.rb
+++ db/migrate/20190822171123_create_users.rb
@@ -1,9 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
+  # Миграция, создающая сущность (таблицу) пользователя в базе.
   def change
+    # Создает таблицу "users"
     create_table :users do |t|
+      # Со строковыми полями name, username
       t.string :name
       t.string :username
 
+      # И created_at (время создания записи), updated_at (время последнего обновления записи).
       t.timestamps
     end
   end
--- db/migrate/20190822174018_create_questions.rb
+++ db/migrate/20190822174018_create_questions.rb
@@ -1,9 +1,13 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
+  # Миграция, создающая сущность (таблицу) вопрос в базе.
   def change
+    # Создает таблицу "questions"
     create_table :questions do |t|
+      # Со строковыми полями text, answer
       t.string :text
       t.string :answer
 
+      # И created_at (время создания записи), updated_at (время последнего обновления записи).
       t.timestamps
     end
   end
