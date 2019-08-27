f4b7524f9a9a967162fcc759ea75009d534ae394
Alex Smirnov <sas2job@gmail.com>
2019-08-27 16:21:27 +0300

    Added validation for uniqueness fields email, username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   # Валидация, которая проверяет, что поля email и username не пустые и не равны
   # nil. Если не задан email и username, объект не будет сохранен в базу.
   validates :email, :username, presence: true
+
+  # Валидация, которая проверяет уникальность полей email и username. Если в
+  # базе данных уже есть записи с такими email и/или username, объект не будет
+  # сохранен в базу.
+  validates :email, :username, uniqueness: true
 end
