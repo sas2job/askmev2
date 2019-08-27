e049bbebef586647e2c3f0802e573d4a47f45ae7
Alex Smirnov <sas2job@gmail.com>
2019-08-27 16:11:44 +0300

    Added validation for empty fields email, username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@
 # Каждый экземпляр этого класса — загруженная из БД инфа о конкретном юзере.
 
 class User < ApplicationRecord
+  # Валидация, которая проверяет, что поля email и username не пустые и не равны
+  # nil. Если не задан email и username, объект не будет сохранен в базу.
+  validates :email, :username, presence: true
 end
