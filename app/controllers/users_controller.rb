4ab71bd97f6250d03694078e7ecd511e0ea4e381
Alex Smirnov <sas2job@gmail.com>
2019-08-27 21:20:59 +0300

    Overwritten controller users and created its views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-    def show
-        @time = Time.now
-        @hello = "Привет!"
-    end
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
 end
