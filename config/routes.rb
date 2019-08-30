fbb090d6d2929cbb8330e9441a10938db3f53cf4
Alex Smirnov <sas2job@gmail.com>
2019-08-30 22:29:44 +0300

    Added a comments for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # Это правило говорит: если пользователь заходит по адресу /, направь его в
+  # контроллер users, действие index. Грубо говоря, на главной странице у нас
+  # список юзеров.
   root to: 'users#index'
+
+  # Эти две строчки добавляют ресурсы для пользователей и вопросов. Ресурс — это
+  # набор путей для управления соответствующей моделью. 
   resources :users
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
