948590ca5614213adab508ae64b0a2e8b968be38
Alex Smirnov <sas2job@gmail.com>
2019-09-02 14:49:25 +0300

    Updated action create for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,25 @@
+# Контроллер, управляющий сессиями. Умеет:
+#
+# 1. Показывает страницу логина
+# 2. Создает новую сессию (логин)
+# 3. Позволяет разлогиниваться (удаляет сессию)
 class SessionsController < ApplicationController
+  # Пустой экшен, только показывает свой шаблон
   def new
   end
 
+  # Создает в объекте session новый факт залогиненности пользователя, если он
+  # правильно сообщил мэйл/пароль
   def create
+    @user = User.authenticate(params[:email], params[:password])
+
+    if @user.present?
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'вы успешно залогинились'
+    else
+      flash.now.alert = 'Неправильный мэйл или пароль'
+      render :new
+    end
   end
 
   def destroy
