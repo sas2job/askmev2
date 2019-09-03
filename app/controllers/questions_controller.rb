bd6f49d14c7c93d62555eb30fd7ac324dffd1cc3
Alex Smirnov <sas2job@gmail.com>
2019-09-03 08:21:04 +0300

    Added controller Questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,57 @@
+class QuestionsController < ApplicationController
+  
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
+
+  # GET /questions
+  def index
+    @questions = Question.all
+  end
+
+  # GET /questions/1
+  def show
+  end
+
+  # GET /questions/new
+  def new
+    @question = Question.new
+  end
+
+  # GET /questions/1/edit
+  def edit
+  end
+
+  # POST /questions
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question, notice: 'Question was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /questions/1
+  def update
+    if @question.update(question_params)
+      redirect_to @question, notice: 'Question was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /questions/1
+  def destroy
+    @question.destroy
+    redirect_to questions_url, notice: 'Question was successfully destroyed.'
+  end
+
+  private
+    def load_question
+      @question = Question.find(params[:id])
+    end
+
+    def question_params
+      params.require(:question).permit(:create, :edit, :update, :destroy)
+    end
+end
