b48c742f311966aaf5b6a5cf43d8731b1d464cca
Alex Smirnov <sas2job@gmail.com>
2019-09-22 18:41:40 +0300

    Added downcase for hashtag

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -3,7 +3,13 @@ class Hashtag < ApplicationRecord
     has_many :hashtags_questions
     has_many :questions, through: :hashtags_questions
 
+    before_validation :downcase_hastags
+
     validates :name, uniqueness: true
+
+    def downcase_hastags
+        self.name.downcase!
+      end
 
     def to_param
         name
